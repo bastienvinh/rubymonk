af3d16cef863b878564068ffa9fae0a4576b7357
Bastien VINH <bvinh.pro@gmail.com>
2017-04-12 00:22:36 +0100

    Rubymonk solution sucks

--- non_duplicated_values.rb
+++ non_duplicated_values.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 def non_duplicated_values(values)
   res = []
   els = values.sort
@@ -8,3 +10,19 @@ def non_duplicated_values(values)
   end
   res
 end
+
+
+def non_duplicated_values2(values)
+  values.find_all { |x| values.count(x) == 1 }
+end
+
+
+def generate_rand_tab
+  res = []
+  (0..10000000).each { |i| res[i] = rand(1000).to_i }
+  res
+end
+
+res = generate_rand_tab
+puts Benchmark.measure { non_duplicated_values(res) }
+puts Benchmark.measure { non_duplicated_values2(res) }
