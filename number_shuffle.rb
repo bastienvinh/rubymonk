0354349c853655e1adc5f7081ab93f07d64a1723
Bastien VINH <bvinh.pro@gmail.com>
2017-04-12 19:01:12 +0100

    Optmisation number_shuffle

--- number_shuffle.rb
+++ number_shuffle.rb
@@ -1,4 +1,25 @@
+require 'Benchmark'
+require 'pp'
+
+# finally beats the algorithm of rubymonk ><''
+
 def number_shuffle(number)
-  sample = number.to_s.split('')
-  sample.permutation(sample.length).to_a.collect { |a| a.join('').to_i }.sort
+  res = []
+  sample = number.to_s
+  sample.chars.combination(sample.length).each { |a| res << a.join.to_i }
+  res
 end
+
+# Ruby monk solutions
+
+def number_shuffle2(number)
+  no_of_combinations = number.to_s.size == 3 ? 6 : 24
+  digits = number.to_s.split(//)
+  combinations = []
+  combinations << digits.shuffle.join.to_i while combinations.uniq.size!=no_of_combinations
+  combinations.uniq.sort
+end
+
+
+puts Benchmark.measure { number_shuffle(1234567890) }
+puts Benchmark.measure { number_shuffle2(1234567890) }
